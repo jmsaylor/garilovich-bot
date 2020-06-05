cbe22de45471f36c4518c10a563e83ff88c2de26
John <johnmsaylor@gmail.com>
2020-06-05 09:33:45 -0400

    minimim config

---
+++ app.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { prefix, token } = require("./config");
+
+client.login(token);
+
+client.on("ready", () => {
+  console.log(`You are ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "gary") {
+    msg.reply("baoo baoo");
+  }
+});
